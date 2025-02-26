efd84875f669cb622744e024a7bbad1102b4d30c
KemaAlexArt <sandskyart@gmail.com>
2025-02-26 14:55:26 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# Запускаем ollama в фоновом режиме
-ollama serve &
-OLLAMA_PID=$!
-
-# Ждем запуска сервиса
-sleep 5
-
 # Загружаем модели (раскомментируйте нужные)
 # ollama pull llama2
 # ollama pull mistral
 # ollama pull gemma:7b
 
-# Остаемся в активном состоянии и перенаправляем логи
-wait $OLLAMA_PID
+# Запускаем ollama на переднем плане
+ollama serve
